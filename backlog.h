0c34d7d8a144c352f19d9be02bfb1293d23d5eea
李云龙 <liyunlong@jovision.com>
2017-07-20 22:46:59 +0800

    backlog parameter

---
+++ backlog.h
@@ -0,0 +1,56 @@
+//
+// Created by lee on 17-7-20.
+//
+
+#ifndef LBSP_BACKLOG_H
+#define LBSP_BACKLOG_H
+
+#include <iostream>
+#include <sys/socket.h>
+#include <csignal>
+#include <cassert>
+#include <cstring>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+static bool stop = false;
+
+static void handle_term(int sig)
+{
+    stop = true;
+}
+
+static void print_usage(const std::string& base_name)
+{
+    std::cout << "Usage:\n" << base_name
+              << " ip port backlog" << std::endl;
+}
+
+static void create_server(const std::string& ip, const int port, const int backlog)
+{
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    assert(sock >= 0);
+
+    // create IPv4 socket address
+    struct sockaddr_in address;
+    bzero(&address, sizeof(address));
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET, ip.c_str(), &address.sin_addr);
+    address.sin_port = htons((unsigned short)port);
+
+    int ret = bind(sock, (struct sockaddr*)&address, sizeof(address));
+    assert(ret != -1);
+
+    ret = listen(sock, backlog);
+    assert(ret != 1);
+
+    while (!stop)
+    {
+        sleep(1);
+    }
+
+    close(sock);
+    return;
+}
+#endif //LBSP_BACKLOG_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "backlog.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+int main(int argc, char* argv[]) {
+    signal(SIGTERM, handle_term);
+
+    if (argc < 3)
+    {
+        print_usage(argv[0]);
+    }
+
+    create_server(argv[1], atoi(argv[2]), atoi(argv[3]));
+
     return 0;
 }
